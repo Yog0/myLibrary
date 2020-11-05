e6421921a53610342895af72fb21e71476c68b5f
Yog0 <oc.miles@gmail.com>
2020-11-05 14:09:51 +1100

    updates to html

--- books.js
+++ books.js
@@ -1,4 +1,11 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R Tolkien',
+        pages: '265',
+        read: 'not read yet'
+    }
+];
 
 function Book(title, author, pages, read){
     this.title = title,
@@ -13,3 +20,34 @@ function Book(title, author, pages, read){
 function addBookToLibrary () {
     // todo
 }
+
+function displayBooks () {
+    // todo
+}
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+  }
+
+  function generateTable(table, data) {
+      for (let element of data) {
+          let row = table.insertRow();
+          for (key in element) {
+              let cell = row.insertCell();
+              let text = document.createTextNode(element[key]);
+              cell.appendChild(text);
+          }
+      }
+  }
+
+  let table = document.querySelector("table");
+  let data = Object.keys(myLibrary[0])
+  generateTable(table, myLibrary);
+  generateTableHead(table, data);
